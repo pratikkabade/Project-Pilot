7686d7de409dde9a0a5040abf3e1c42c795ae975
Prãt!k Kàbádé <pratikkabade@hotmail.com>
2024-06-20 14:00:00 +0100

    bug-fix: pageName auto-change

--- src/components/layout/AppNavbar.tsx
+++ src/components/layout/AppNavbar.tsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
 import { ThemeToggler } from "../ThemeToggler";
 import LogoSVG from "../../assets/logo/LogoSVG";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { GetItem, SetItem } from "../../functions/ArrayData";
 import PageName from "../../functions/PageName";
 
 export const AppNavbar = () => {
      const [id, setId] = useState(GetItem('login_details'));
 
-     const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
+     useEffect(() => {
+          PageName('Project-Pilot');
+     }, []);
 
      useEffect(() => {
-          PageName('Project-Pilot')
           console.log(`ID has changed to: ${id}`);
-          forceUpdate();
-     }, [id, ignored]);
+     }, [id]);
+
+     const handleSignOut = () => {
+          SetItem('login_details', '');
+          setId('');
+     };
 
      return (
           <div className="navbar fixed z-50 shadow-xl backdrop-blur-sm bg-base-100 rounded-b-3xl px-4 pr-6">
@@ -32,18 +37,12 @@ export const AppNavbar = () => {
                     <ul className="px-1 items-center mr-2 font-bold">
                          <li className="px-3">
                               {
-                                   id === '' ? (
-                                        <Link to={'/Login'}
-                                             className="hover:text-blue-600 hover:bg-base-100">
+                                   !id ? (
+                                        <Link to={'/Login'} className="hover:text-blue-600 hover:bg-base-100">
                                              Login
                                         </Link>
                                    ) : (
-                                        <Link to={'/Login'}
-                                             onClick={() => {
-                                                  SetItem('login_details', '')
-                                                  setId('')
-                                             }}
-                                             className="hover:text-red-600 hover:bg-base-100">
+                                        <Link to={'/Login'} onClick={handleSignOut} className="hover:text-red-600 hover:bg-base-100">
                                              Sign Out
                                         </Link>
                                    )
@@ -53,6 +52,5 @@ export const AppNavbar = () => {
                     <ThemeToggler />
                </div>
           </div>
-
      );
 }
