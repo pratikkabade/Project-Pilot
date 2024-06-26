4484b917534364418fb4e23efee7f22222f75af6
Prãt!k Kàbádé <pratikkabade@hotmail.com>
2024-06-26 14:00:00 +0530

    update: change-task-status

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import TaskInterface from "../interfaces/TaskInterface";
 import EmployeeInterface from "../interfaces/EmployeeInterface";
+import { Select } from "flowbite-react";
+import StatusOptions from "../interfaces/StatusOptions";
+import PageName from "../functions/PageName";
+import { GetItem, SetItem } from "../functions/ArrayData";
 
 export const Employee = () => {
     const [tasks, setTasks] = useState<TaskInterface[]>([]);
@@ -8,22 +12,33 @@ export const Employee = () => {
     const employee_id = 'e2';
 
     useEffect(() => {
-        const employees: EmployeeInterface[] = JSON.parse(localStorage.getItem('employees') || '[]');
+        const employees: EmployeeInterface[] = GetItem('employees');
         setEmployees(
             employees
                 .filter(
                     (employee: EmployeeInterface) =>
                         employee.employee_id === employee_id
                 ));
-        const tasks: TaskInterface[] = JSON.parse(localStorage.getItem('tasks') || '[]');
+        const tasks: TaskInterface[] = GetItem('tasks');
         setTasks(tasks
             .filter(
                 (task: TaskInterface) =>
                     task.employee_id === employee_id
             ));
+        PageName('Employee');
     }, []);
 
-    console.log(tasks);
+    // function to change status
+    const changeStatus = (task_id: number, status: string) => {
+        const newTasks = tasks.map((task: TaskInterface) => {
+            if (task.task_id === task_id) {
+                task.status = status;
+            }
+            return task;
+        });
+        setTasks(newTasks);
+        SetItem('tasks', newTasks);
+    };
 
     return (
         <div>
@@ -37,12 +52,49 @@ export const Employee = () => {
             </ul>
             <h1>Tasks</h1>
             <ul>
-                {tasks
-                    .map((task: TaskInterface) => (
-                        <div key={task.task_id} className='flex flex-col bg-emerald-50 m-3'>
-                            {task.title} | {task.description} | {task.status}
-                        </div>
-                    ))}
+                {
+                    tasks
+                        .filter((task: TaskInterface) => task.status !== 'completed')
+                        .map((task: TaskInterface) => (
+                            <div key={task.task_id} className={`flex flex-col m-3 
+                        ${task.status === 'in-progress' ? '' :
+                                    task.status === 'pending' ? '' : ''} 
+                        `}>
+                                {task.title} | {task.description} | {task.status}
+                                <Select
+                                    className='w-1/4'
+                                    value={task.status}
+                                    onChange={(e) => changeStatus(task.task_id, e.target.value)}
+                                >
+                                    {StatusOptions.map((status) => (
+                                        <option key={status} value={status}>
+                                            {status}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </div>
+                        ))
+                }
+                {
+                    tasks
+                        .filter((task: TaskInterface) => task.status === 'completed')
+                        .map((task: TaskInterface) => (
+                            <div key={task.task_id} className='flex flex-col m-3 '>
+                                {task.title} | {task.description} | {task.status}
+                                <Select
+                                    className='w-1/4'
+                                    value={task.status}
+                                    onChange={(e) => changeStatus(task.task_id, e.target.value)}
+                                >
+                                    {StatusOptions.map((status) => (
+                                        <option key={status} value={status}>
+                                            {status}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </div>
+                        ))
+                }
             </ul>
         </div>
     );
